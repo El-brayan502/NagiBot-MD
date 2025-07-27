2755eba15675d2fedf47dbd22f7dc3051fad5a47
𝑨𝑰𝑵𝒁 𝑶𝑶𝑨𝑳 𝑮𝑶𝑾𝑵 <brayanphonkponk@gmail.com>
2025-07-27 17:52:27 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -107,8 +107,6 @@ global.gp1 = 'https://chat.whatsapp.com/HACwRMduEef1DKXPWQoJ9j';
 global.channel = 'https://chat.whatsapp.com/GDUv1z6UG0k2xe8zAEUnFf';
 global.md = 'https://chat.whatsapp.com/GDUv1z6UG0k2xe8zAEUnFf';
 global.correo = 'davi.zuni.17@gmail.com';
-global.cn = 'https://chat.whatsapp.com/GDUv1z6UG0k2xe8zAEUnFf';
-global.instagram = 'https://chat.whatsapp.com/GDUv1z6UG0k2xe8zAEUnFf';
 global.whatsApp = 'https://chat.whatsapp.com/GDUv1z6UG0k2xe8zAEUnFf';
 global.correo = 'davi.zuni.17@gmail.com';
 
