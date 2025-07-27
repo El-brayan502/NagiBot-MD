5e75f5f605c2a416c9572f5dbccef2aa723b0544
𝑨𝑰𝑵𝒁 𝑶𝑶𝑨𝑳 𝑮𝑶𝑾𝑵 <brayanphonkponk@gmail.com>
2025-07-27 17:56:09 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -104,10 +104,10 @@ global.photoSity = [catalogo2]
 //✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 
 global.gp1 = 'https://chat.whatsapp.com/HACwRMduEef1DKXPWQoJ9j';
-global.channel = 'https://whatsapp.com/channel/0029Vb6BDQc0lwgsDN1GJ31i';
 global.md = 'https://chat.whatsapp.com/GDUv1z6UG0k2xe8zAEUnFf';
+global.channel = 'https://whatsapp.com/channel/0029Vb6BDQc0lwgsDN1GJ31i';
+global.channel2 = 'https://whatsapp.com/channel/0029VajUPbECxoB0cYovo60W';
 global.correo = 'brayanphonkponk@gamail.com';
-global.whatsApp = 'https://whatsapp.com/channel/0029VajUPbECxoB0cYovo60W';
 global.correo = 'brayanfree881@gmail.com';
 
 //✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
